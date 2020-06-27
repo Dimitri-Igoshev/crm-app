9388fb067041a0821523a1fad2c889bfc6f8c77b
Dimitri-Igoshev <igoscheff@gmail.com>
2020-06-27 15:10:20 +0200

    Models were created and a remote database was connected.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const keys = require('./config/keys')
 
-const analyticsRoutes = require('./routes/analytics.router')
+const analyticsRoutes = require('./routes/analytics.routes')
 const authRoutes = require('./routes/authorization.routes')
 const categoryRoutes = require('./routes/category.routes')
 const orderRoutes = require('./routes/order.routes')
 const positionRoutes = require('./routes/position.routes')
 
 const app = express()
+
+mongoose.connect(keys.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
+	.then(() => console.log('MongoDB connected...'))
+	.catch(error => console.log(error))
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://crm-app:7VxRjJ03WBe3ndt0@cluster0-f3dnf.mongodb.net/crm-app'
+}
